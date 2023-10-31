06404f06c1fc3f231f15e66cf318c5fe466aeabd
wafer-bw <ben.jz.wakeford@gmail.com>
2023-10-31 19:13:19 -0400

    Add TODO

--- doc.go
+++ doc.go
@@ -4,3 +4,4 @@ package memcache
 // TODO:
 // - Separate concerns of store vs top level cache. Is the cache just a wrapper
 //   or does it have its own behavior to add?
+// - Add more eviction policies.
