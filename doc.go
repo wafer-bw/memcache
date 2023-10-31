34e2cb87d68d8c53bbc41cc4dafc8f050f3eee21
wafer-bw <ben.jz.wakeford@gmail.com>
2023-10-31 18:38:07 -0400

    Remove TODO that cannot be done

--- doc.go
+++ doc.go
@@ -3,6 +3,5 @@ package memcache
 
 // TODO:
 // - Determine if cache & closer need to be constructed as pointers or not.
-// - Update tests to use Items() to set state instead of exported methods.
 // - Separate concerns of store vs top level cache. Is the cache just a wrapper
 //   or does it have its own behavior to add?
