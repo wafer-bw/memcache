af04cad902f3ed575830d171f529efbe6f8d09bb
wafer-bw <ben.jz.wakeford@gmail.com>
2024-02-17 23:01:12 -0500

    Add tests for closeable

---
+++ internal/store/closeable/closeable_test.go
@@ -0,0 +1,41 @@
+package closeable_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/wafer-bw/memcache/internal/store/closeable"
+)
+
+func TestClose(t *testing.T) {
+	t.Parallel()
+
+	t.Run("subsequent calls do not panic", func(t *testing.T) {
+		t.Parallel()
+
+		c := closeable.New()
+		c.Close()
+		c.Close()
+	})
+}
+
+func TestClosed(t *testing.T) {
+	t.Parallel()
+
+	t.Run("returns false when not closed", func(t *testing.T) {
+		t.Parallel()
+
+		c := closeable.New()
+		close := c.Closed()
+		require.False(t, close)
+	})
+
+	t.Run("returns true when closed", func(t *testing.T) {
+		t.Parallel()
+
+		c := closeable.New()
+		c.Close()
+		closed := c.Closed()
+		require.True(t, closed)
+	})
+}
